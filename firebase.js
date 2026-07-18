b46d9d7a6f2d37c3d2ecd6633aafbc893467a0c3
kookvkpop <kookv3001.1209@gmail.com>
2026-07-19 04:47:30 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCmOeijeREN2uLAvOgo0oaiV6mpuYLfqxk",
+  authDomain: "kookvkpop-190dd.firebaseapp.com",
+  projectId: "kookvkpop-190dd",
+  storageBucket: "kookvkpop-190dd.firebasestorage.app",
+  messagingSenderId: "1034083891854",
+  appId: "1:1034083891854:web:325812ee28e6550d9967df",
+  measurementId: "G-N5QNTN5Y2S"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
