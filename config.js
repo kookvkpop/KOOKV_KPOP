01b5ed14a2eb271ee967fdfbf0f8c10779de08a0
kookvkpop <kookv3001.1209@gmail.com>
2026-07-26 00:34:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+
+API_URL:
+"https://script.google.com/macros/s/AKfycbwdXnekkPNNav1PbQMt8s01dReumZxkv3c34i6n_xUBjMAmjKSnm1NBtpFvzq7yJtcc/exec",
+
+SHOP_NAME:"KOOKV_KPOP",
+
+LINE_URL:"",
+INSTAGRAM_URL:""
+
+};
